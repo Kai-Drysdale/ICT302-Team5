f529057d8ed4ef61560ad8ddd1fcc8f90264ae63
Kai-Drysdale <132035721+Kai-Drysdale@users.noreply.github.com>
2023-09-11 14:09:31 +0800

    Create real_time.js
    added javascript file

---
+++ onlinetexte/js/real_time.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", function() 
+{
+    var textArea = document.getElementById("id_onlinetexte_editor");
+    var wordCountDisplay = document.getElementById("word_count_display");
+    var timerDisplay = document.getElementById("timer_display");
+
+    function countWords(text) 
+    {
+        var withoutTags = text.replace(/<\/?[^>]+(>|$)/g, "");
+        var wordCount = withoutTags.match(/\b\w+\b/g);
+        return wordCount ? wordCount.length : 0;
+    }
+
+    function updateWordCount() 
+    {
+        var content = textArea.value;
+        var wordCount = countWords(content);
+        wordCountDisplay.textContent = "Word Count: " + wordCount;
+    }
+
+    function updateTimer() 
+    {
+        var now = new Date();
+        var timeRemaining = duedate - now;
+    
+        if (timeRemaining > 0) 
+        {
+            var hours = Math.floor(timeRemaining / (1000 * 60 * 60));
+            var minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+            timerDisplay.textContent = "Time Remaining: " + hours + " hours " + minutes + " minutes";
+        } else 
+        {
+            var hours = Math.floor(-timeRemaining / (1000 * 60 * 60));
+            var minutes = Math.floor((-timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+            timerDisplay.textContent = "Overdue by: " + hours + " hours " + minutes + " minutes";
+        }
+    }
+
+    setInterval(updateWordCount, 10);
+    setInterval(updateTimer, 1000);
+
+    updateWordCount();
+    updateTimer();
+});
