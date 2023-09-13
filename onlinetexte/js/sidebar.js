cc30c0d6b0b43326e6377f654810e7ddabf1528c
Kai-Drysdale <132035721+Kai-Drysdale@users.noreply.github.com>
2023-09-13 14:41:35 +0800

    added javascript for side bar

---
+++ onlinetexte/js/sidebar.js
@@ -0,0 +1,10 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const alertLink = document.getElementById("sidebarbutton");
+    const sidebar = document.getElementById("sidebar");
+  
+    alertLink.addEventListener("click", function (e) {
+      e.preventDefault(); 
+      sidebar.classList.toggle("active"); 
+    });
+  });
